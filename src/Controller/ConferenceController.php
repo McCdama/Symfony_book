11abe45a61b02a19b64c5cb20687b8f425da41e9
Mohed <mccdama@gmail.com>
2021-07-14 15:53:19 +0200

    Paginating the Comments

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -7,6 +7,7 @@ use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
 /* We don’t even need to extend the AbstractController class if we want to be explicit about our dependencies. */
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
@@ -22,20 +23,28 @@ class ConferenceController extends AbstractController
     }
 
 
+
+    /* To manage the pagination in the template,
+    * Pass the Doctrine Paginator instead of the Doctrine Collection to Twig: */
+
     /**
      * @Route("/conference/{id}", name="conference")
      */
-    public function show(Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Environment $twig, Conference $conference, CommentRepository $commentRepository): Response
     {
+
+        /* gets the offset from the Request query string ($request->query) as an integer (getInt()), defaulting to 0 if not available. */
+        $offset = max(0, $request->query->getInt('offset', 0));
+        $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+
         return new Response($twig->render(
             'conference/show.html.twig',
             [
                 'conference' => $conference,
-                'comments' => $commentRepository
-                    ->findBy(
-                        ['conference' => $conference],
-                        ['createdAt' => 'DESC']
-                    )
+                'comments' => $paginator,
+                /* The previous and next offsets are computed based on all the information we have from the paginator */
+                'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE)
             ]
         ));
     }

599479428ed3856cd7d293adcdbdee9476f38f84
Mohed Alrahbi <mccdama@gmail.com>
2021-07-15 12:00:02 +0200

    template

---
+++ assets/app.js
@@ -0,0 +1,16 @@
+/*
+ * Welcome to your app's main JavaScript file!
+ *
+ * We recommend including the built version of this JavaScript file
+ * (and its CSS file) in your base layout (base.html.twig).
+ */
+
+
+
+import './bootstrap';
+import bsCustomFileInput from 'bs-custom-file-input'
+
+import './styles/app.scss';
+
+
+bsCustomFileInput.init()
